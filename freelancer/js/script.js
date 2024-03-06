0c42e51cdd76e96e50cf9b47545f029ac69d120f
miftahul munir <miftahulmunir001@gmail.com>
2024-03-06 10:56:26 +0700

    Update script.js

--- freelancer/js/script.js
+++ freelancer/js/script.js
@@ -1,5 +1,37 @@
 addEventListener('load', a => {
 	
-	a = el({a:'div', b:document.body, d:{id:'main-header'} })
+	(a => {
+		
+		el({a:'div', b:a, d:{class:'hero-banner'} });
+		
+		(a => {
+			
+			el({a:'div', b:a, c:'We Work With Intelligent Professionals' })
+			el({a:'p', b:a, c:'Every day, corporations and governments of all sizes depend on us to translate their important written materials, and to provide real-time multilingual interpretation services.' })
+			el({a:'p', b:a, c:'They partner with us because we understand their needs, their concerns, and because of our notable standards for accuracy and efficiency.' })
+			el({a:'p', b:a, c:'Contact us now. You’ll be amazed at how easy it is to work with us!' })
+			el({a:'div', b:a, c:'Delivering translation projects of all scopes and complexity to companies like these since 1987' })
+			
+		})(el({a:'div', b:a, d:{class:'ct1'} }))
+		
+	})(el({a:'div', b:document.body, d:{id:'main'} }));
+	
+	
+	(a => {
+		
+		el({a:'img', b:el({a:'a', b:a, d:{href:'https://asta-usa.com/'} }), d:{src:'img/logo_lightbackground.svg'} });
+		//el({a:'div', b:a});
+		
+		(a => {
+			
+			el({a:'div', b:a, c:'Specialized Translation Services' })
+			el({a:'div', b:a, c:'Who We Work With' })
+			el({a:'div', b:a, c:'U.S. Regions Served' })
+			el({a:'div', b:a, c:'Inquire Now' })
+			el({a:'div', b:a, c:'1.866.446.1860' })
+			
+		})(el({a:'div', b:a }))
+		
+	})(el({a:'div', b:document.body, d:{id:'main-header'} }))
 	
 })
